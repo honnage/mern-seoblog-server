008cdc72035da15bb3583329ccc15cbac2a8c219
honnage <hon_da-1998@hotmail.com>
2022-07-04 16:57:06 +0700

    User schema virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -65,4 +65,38 @@ const userSchema = new mysql.Schema(
   }
 );
 
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password; // create a temporarity variable called _password
+    this.salt = this.makeSalt();  // generate salt
+    this.hashed_password = this.encryptPassword(password);  // encryptPassword
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
 module.exports = mysql.model("User", userSchema);
